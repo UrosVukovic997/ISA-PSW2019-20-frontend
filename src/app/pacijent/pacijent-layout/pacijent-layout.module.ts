55ac812dc55619c9926615615e4c971610cd5a9b
BorislavGajic <57411821+BorislavGajic@users.noreply.github.com>
2020-02-06 16:29:53 +0100

    Profil pacijenta i kraj logovanja

---
+++ src/app/pacijent/pacijent-layout/pacijent-layout.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {PacijentLayoutComponent} from './pacijent-layout.component';
+import {ProfilComponent} from '../profil/profil.component';
+import {PacCompModule} from '../pac-comp/pac-comp.module';
+import {RouterModule} from '@angular/router';
+import {AdminKcLayoutRoutes} from '../../administrator-kc/admin-kc-layout/admin-kc-layout.routing';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {ChartsModule} from 'ng2-charts';
+import {ToastrModule} from 'ngx-toastr';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {ComponentsModule} from '../../administrator-kc/components/components.module';
+import {DropdownListModule} from 'ngx-dropdown-list';
+import {PacijentLayoutRutes} from './pacijent-layout.routing';
+
+
+
+@NgModule({
+  declarations: [
+    PacijentLayoutComponent,
+    ProfilComponent
+  ],
+  imports: [
+    CommonModule,
+    PacCompModule,
+    RouterModule.forChild(PacijentLayoutRutes),
+    FormsModule,
+    ChartsModule,
+    ToastrModule.forRoot(),
+    NgbModule,
+    ReactiveFormsModule,
+    DropdownListModule
+  ],
+  exports: [PacijentLayoutComponent
+  ]
+})
+export class PacijentLayoutModule { }
---
+++ src/app/pacijent/profil/profil.component.ts
@@ -0,0 +1,92 @@
+import { Component, OnInit } from '@angular/core';
+import {RegZahtrviService} from '../../service/administrator-kc-service/reg-zahtrvi.service';
+import {Router} from '@angular/router';
+import {ProfilPacijentaService} from '../../service/pacijent-service/profil-pacijenta.service';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {first} from 'rxjs/operators';
+import {PacijentEdit} from '../../shared/utilities/pacijent_edit';
+
+@Component({
+  selector: 'app-profil',
+  templateUrl: './profil.component.html',
+  styleUrls: ['./profil.component.css']
+})
+export class ProfilComponent implements OnInit {
+  registerForm: FormGroup;
+  loading = false;
+  submitted = false;
+  error: string;
+
+
+  id: number;
+  pacijent;
+  constructor(private pps: ProfilPacijentaService, private router: Router, private formBuilder: FormBuilder) { }
+
+  ngOnInit( ) {
+    this.ucitajZahteve();
+    this.registerForm = this.formBuilder.group({
+      imePacijenta: ['', Validators.required],
+      prezimePacijenta: ['', Validators.required] ,
+      adresa: ['', Validators.required],
+      grad: ['', Validators.required],
+      drzava: ['', Validators.required],
+      brojTelefona: ['', Validators.required],
+      username: ['', Validators.required],
+      rodjen: ['', Validators.required]
+    });
+  }
+
+  ucitajZahteve() {
+    this.pps.getByUsername(localStorage.getItem('currentUserUsername').toString())
+      .subscribe((data: {}) => {
+          this.pacijent = data;
+          console.log(this.pacijent);
+        }
+      );
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    console.log('ulaz');
+    // stop here if form is invalid
+    /*if (this.registerForm.invalid) {
+      console.log('losa');
+      return;
+    }*/
+
+    const imePacijenta = ((document.getElementById('imePacijenta') as HTMLInputElement).value);
+    const prezimePacijenta = ((document.getElementById('prezimePacijenta') as HTMLInputElement).value);
+    const adresa = ((document.getElementById('adresa') as HTMLInputElement).value);
+    const grad = ((document.getElementById('grad') as HTMLInputElement).value);
+    const drzava = ((document.getElementById('drzava') as HTMLInputElement).value);
+    const brojTelefona = ((document.getElementById('brojTelefona') as HTMLInputElement).value);
+    const username = ((document.getElementById('username') as HTMLInputElement).value);
+    const rodjen = ((document.getElementById('rodjen') as HTMLInputElement).value);
+    const email = ((document.getElementById('email') as HTMLInputElement).value);
+
+    const pac = new PacijentEdit();
+    pac.imePacijenta = imePacijenta;
+    pac.prezimePacijenta = prezimePacijenta;
+    pac.adresa = adresa;
+    pac.grad = grad;
+    pac.drzava = drzava;
+    pac.brojTelefona = brojTelefona;
+    pac.username = username;
+    pac.rodjen = rodjen;
+    pac.email = email;
+
+    this.loading = true;
+    this.pps.izmeni(pac)
+      .pipe(first())
+      .subscribe(
+        data => {
+          this.router.navigate(['/profilPacijenta/profil'], { queryParams: { registered: true }});
+          this.loading = false;
+        },
+        error => {
+          this.error = error;
+          this.loading = false;
+        });
+  }
+
+}
